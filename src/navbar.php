0562ab5947ccf7bac6e8dc1744e86cf3d877eb58
osa623 <it22170798@my.sliit.lk>
2024-09-29 15:02:50 +0530

    commit navbar change

--- src/navbar.php
+++ src/navbar.php
@@ -1,6 +1,6 @@
 <?php
 
-
+session_start(); 
 // Include your database connection file
 require('db.php'); // Ensure db.php sets up a MySQLi connection
 
@@ -86,6 +86,9 @@ if (isset($_POST['logout'])) {
                     </li>
                     <li>
                         <a href="view_schedule.php" style="text-decoration: none; color: white; font-weight: 600; padding: 10px 15px; border-radius: 5px; transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s;">Schedules</a>
+                    </li>
+                    <li>
+                        <a href="view_schedule.php" style="text-decoration: none; color: white; font-weight: 600; padding: 10px 15px; border-radius: 5px; transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s;">Workout Schedules</a>
                     </li>
                     <li>
                         <a href="" style="text-decoration: none; color: white; font-weight: 600; padding: 10px 15px; border-radius: 5px; transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s;">Tips</a>
